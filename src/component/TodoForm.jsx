d4ac9471f97234298d2fe7ce12115cd215f5e9ab
Adarsh Maurya <adarshmaurya0022@gmail.com>
2024-08-23 11:26:41 +0530

    todo done

--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -17,7 +17,7 @@ function TodoForm() {
   return (
     <form onSubmit={add} className="mb-10 flex md:flex-row flex-col md:gap-0 gap-3 items-center justify-between">
       <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} className="focus:outline-2 focus:outline-black outline-offset-2 border md:w-[75%] w-full text-lg font-light rounded-xl outline-none py-2 px-4" placeholder="Write Todo..." />
-      <button className="border px-6 py-2 rounded-xl bg-black md:w-auto w-full transition-all text-white text-lg">+ Add</button>
+      <button className="border px-6 focus:ring-4 focus:ring-gray-300 py-2 rounded-xl bg-black md:w-auto w-full transition-all text-white text-lg">+ Add</button>
     </form>
   )
 }
